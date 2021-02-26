bee4f52965d3e7145a4f4ca5abf378e293f6973b
Abrar Faisal <abrarfaisal20@gmail.com>
2021-02-26 13:34:07 +0100

    Update ImageBoard with image upload

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,10 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        title: "",
+        description: "",
+        username: "",
+        file: null,
     },
     mounted: function () {
         var self = this;
@@ -17,5 +21,30 @@ new Vue({
                 console.log("Error:", err.message);
             });
     },
-    methods: {},
+    methods: {
+        handleClick: function (e) {
+            var formData = new FormData();
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            formData.append("file", this.file);
+            // console.log("this.title: ", this.title);
+            // console.log("this.description: ", this.description);
+            // console.log("this.username: ", this.username);
+            // console.log("this.file: ", this.file);
+            axios
+                .post("/uplaod", formData)
+                .then(function (response) {
+                    console.log("Response from post req:", response);
+                })
+                .catch(function (err) {
+                    console.log("Error from post req:", err.message);
+                });
+        },
+        handleChange: function (e) {
+            console.log("e.target.files[0]:", e.target.files[0]);
+            console.log("handle change is running!");
+            this.file = e.target.files[0];
+        },
+    },
 });
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.use(express.static("public"));
 
@@ -13,6 +34,21 @@ app.get("/images", (req, res) => {
         .catch((err) => {
             console.log("Error:", err.message);
         });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("hit the post route....");
+    console.log("req.file: ", req.file);
+    console.log("req.body: ", req.body);
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("🖼️  IB server (port: 8080) online..."));
